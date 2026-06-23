c38831d3ab172089513470a79faf0e7727045d61
Eduard Evdokimov <eduard.evdokimov@inbox.ru>
2026-06-23 09:36:57 +0300

    add migrations

---
+++ src/main/resources/db/changelog/create_users_and_notes_tables.sql
@@ -0,0 +1,26 @@
+--liquibase formatted sql
+
+--changeset eduard:1 logicalFilePath:create_users_and_notes_tables.sql
+--comment: /* Create users and notes tables */
+
+CREATE TABLE users (
+    id SERIAL PRIMARY KEY,
+    login VARCHAR(50) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL,
+    created_at TIMESTAMP NOT NULL,
+    updated_at TIMESTAMP NOT NULL,
+    deleted_at TIMESTAMP
+);
+
+CREATE TABLE notes (
+    id SERIAL PRIMARY KEY,
+    user_id INT NOT NULL REFERENCES users(id),
+    title VARCHAR(255) NOT NULL,
+    is_picked BOOLEAN DEFAULT FALSE,
+    is_archive BOOLEAN DEFAULT FALSE,
+    created_at TIMESTAMP NOT NULL,
+    updated_at TIMESTAMP NOT NULL,
+    deleted_at TIMESTAMP
+);
+
+--rollback drop table notes; drop table users;
